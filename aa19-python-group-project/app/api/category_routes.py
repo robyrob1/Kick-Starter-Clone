aef136f87a782b50362faf5d67469fb6ecaf5376
Dedrickj1 <dedrickjackson11@gmail.com>
2025-07-21 19:31:22 -0500

    update

--- aa19-python-group-project/app/api/category_routes.py
+++ aa19-python-group-project/app/api/category_routes.py
@@ -1,4 +1,12 @@
+from flask import Blueprint, jsonify
+from app.models import Category
+
+category_routes = Blueprint('categories', __name__)
+
 @category_routes.route('/')
 def get_categories():
+    """
+    Get all categories
+    """
     categories = Category.query.all()
-    return [category.to_dict() for category in categories]
+    return jsonify([category.to_dict() for category in categories])
