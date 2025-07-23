9ff4da47b744bc86fce752be59625475abbbba90
Dedrickj1 <dedrickjackson11@gmail.com>
2025-07-23 09:53:48 -0500

    update

---
+++ aa19-python-group-project/migrations/versions/934a8f3ab090_merge_branches.py
@@ -0,0 +1,24 @@
+"""Merge branches
+
+Revision ID: 934a8f3ab090
+Revises: ffdc0a98111c, b964a898c501
+Create Date: 2025-07-22 22:45:55.776263
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '934a8f3ab090'
+down_revision = ('ffdc0a98111c', 'b964a898c501')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
