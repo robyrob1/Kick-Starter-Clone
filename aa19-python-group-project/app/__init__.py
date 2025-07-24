2abab41cefe82e544a2c10f0f412345f2290f9ec
Dedrickj1 <dedrickjackson11@gmail.com>
2025-07-23 20:05:51 -0500

    Revert "Merge branch 'dev'"
    This reverts commit c8a7b469e6277f4e49a08b680924fd4dc54b059b, reversing
    changes made to 28abd6e46462e43a977d97e34d1b0b37fc357af8.

--- aa19-python-group-project/app/__init__.py
+++ aa19-python-group-project/app/__init__.py
@@ -7,10 +7,6 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-from .api.campaign_routes import campaign_routes
-from .api.comment_routes import comment_routes
-from .api.donation_routes import donation_routes
-from .api.category_routes import category_routes
 from .api.project_routes import project_routes
 from .seeds import seed_commands
 from .config import Config
@@ -38,17 +34,8 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(campaign_routes, url_prefix='/api/campaigns')
-app.register_blueprint(comment_routes, url_prefix='/api/comments')
-app.register_blueprint(donation_routes, url_prefix='/api/donations')
 app.register_blueprint(category_routes, url_prefix='/api/categories')
-<<<<<<< HEAD
 # app.register_blueprint(project_categories_bp)
-=======
-app.register_blueprint(project_routes, url_prefix='/api/projects')
-app.register_blueprint(project_categories_bp)
-
->>>>>>> dev
 db.init_app(app)
 Migrate(app, db)
 
