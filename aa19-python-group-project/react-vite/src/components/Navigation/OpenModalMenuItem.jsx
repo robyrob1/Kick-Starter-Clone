a464d72dbe25f8babc41404b263647e52242fb99
Bschaler <90104387+Bschaler@users.noreply.github.com>
2025-07-28 17:39:20 -0500

    Update OpenModalMenuItem.jsx
    for the css additions

--- aa19-python-group-project/react-vite/src/components/Navigation/OpenModalMenuItem.jsx
+++ aa19-python-group-project/react-vite/src/components/Navigation/OpenModalMenuItem.jsx
@@ -15,7 +15,7 @@ function OpenModalMenuItem({
   };
 
   return (
-    <li onClick={onClick}>{itemText}</li>
+    <li className="modal-menu-item-sign-in" onClick={onClick}>{itemText}</li>
   );
 }
 
